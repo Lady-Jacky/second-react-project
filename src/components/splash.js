ba98416b5abebf7337a1afd3fa908a7b0de6b62a
Jaky <114087744+Lady-Jacky@users.noreply.github.com>
2024-01-17 13:51:34 +0000

    feat: Fix CSS formatting, update navbar.js with new navbar design and fix CSS formatting

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './css/splash.css';
+import React from "react";
+import "./css/splash.css";
 
 function Splash() {
-  return(
-    <div className='Splash'>
-      <h1 className='primary text'>Furbook</h1>
-      <h5 className='secondary text'>Where pets aren't just man's best friend</h5>
+  return (
+    <div className="Splash">
+      <h1 className="primary text">Furbook</h1>
+      <h5 className="secondary text">
+        Where pets aren't just man's best friend
+      </h5>
+      <h5 className="secondary text">
+        “The phrase 'domestic cat' is an oxymoron."
+      </h5>
     </div>
   );
 }
